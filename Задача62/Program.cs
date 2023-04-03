0e4330c56eff4a664cdb01b6e3c579a8b7bd538c
Vladislav <galdinsh77@mail.ru>
2023-04-03 13:05:23 +0500

    Задача62

---
+++ Задача62/Program.cs
@@ -0,0 +1,31 @@
+﻿void PrintArray (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      Console.Write($"{matrix[i,j]} \t");
+    Console.WriteLine();
+  }
+}
+Console.Clear();
+Console.Write("Введите размеры двумерного массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+PrintArray(matrix);
