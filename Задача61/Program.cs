b048cde87cac49031ae6a2933221f6b97969cb9f
Vladislav <galdinsh77@mail.ru>
2023-04-06 12:49:24 +0500

    Задача61-2

--- Задача61/Program.cs
+++ Задача61/Program.cs
@@ -1,62 +1,95 @@
-﻿// Взята с лекции
+﻿// // Взята с лекции
 
-int row = 12;
-int[,] triangle = new int[row, row];
-const int cellWidth = 4;
+// int row = 12;
+// int[,] triangle = new int[row, row];
+// const int cellWidth = 4;
 
-void FillTriangle()
-{
-    for (int i = 0; i < row; i++)
-    {
-        triangle[i, 0] = 1;
-        triangle[i, i] = 1;
-    }
+// void FillTriangle()
+// {
+//     for (int i = 0; i < row; i++)
+//     {
+//         triangle[i, 0] = 1;
+//         triangle[i, i] = 1;
+//     }
     
-    for (int i = 2; i < row; i++)
-    {
-        for (int j = 1; j<=i; j++)
-        {
-            triangle[i, j] = 
-                triangle[i - 1, j - 1] + triangle[i - 1, j];
-        }
-    }
-} 
+//     for (int i = 2; i < row; i++)
+//     {
+//         for (int j = 1; j<=i; j++)
+//         {
+//             triangle[i, j] = 
+//                 triangle[i - 1, j - 1] + triangle[i - 1, j];
+//         }
+//     }
+// } 
 
-void PrintTriangle()
+// void PrintTriangle()
+// {
+//     for (int i = 0; i <row; i++)
+//     {
+//         for (int j = 0; j < row; j++)
+//         {
+//             if (triangle[i, j] != 0)
+//                 Console.Write($"{triangle[i, j], cellWidth}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void Magic()
+// {
+//     int col = cellWidth * row;
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             Console.SetCursorPosition(col, i + 1);
+//             if (triangle[i, j] != 0) Console.Write($"{triangle[i, j], cellWidth}");
+//             //if (triangle[i, j] % 2 != 0) Console.WriteLine("*");
+//             //if (triangle[i, j] != 0) Console.WriteLine("*");
+//             col +=cellWidth * 2;
+//         }
+
+//         col = cellWidth * row - cellWidth * (i + 1);
+
+//         Console.WriteLine();
+//     }
+// }
+// Console.Clear();
+// Console.ReadLine();
+// FillTriangle();
+// //PrintTriangle();
+// Console.ReadLine();
+// Magic();
+
+int factorial(int n)
 {
-    for (int i = 0; i <row; i++)
+    int i, x = 1;
+    for (i = 1; i <= n; i++)
     {
-        for (int j = 0; j < row; j++)
-        {
-            if (triangle[i, j] != 0)
-                Console.Write($"{triangle[i, j], cellWidth}");
-        }
-        Console.WriteLine();
+        x *= i;
     }
+    return x;
 }
 
-void Magic()
+
+void ReleaseTask(int n)
 {
-    int col = cellWidth * row;
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < n; i++)
     {
+        for (int j = 0; j <= (n - i); j++)
+        {
+            Console.Write(" ");
+        }
         for (int j = 0; j <= i; j++)
         {
-            Console.SetCursorPosition(col, i + 1);
-            if (triangle[i, j] != 0) Console.Write($"{triangle[i, j], cellWidth}");
-            //if (triangle[i, j] % 2 != 0) Console.WriteLine("*");
-            //if (triangle[i, j] != 0) Console.WriteLine("*");
-            col +=cellWidth * 2;
+            Console.Write(" ");
+            Console.Write(factorial(i) / (factorial(j) * factorial(i - j)));
         }
-
-        col = cellWidth * row - cellWidth * (i + 1);
-
         Console.WriteLine();
     }
 }
-Console.Clear();
-Console.ReadLine();
-FillTriangle();
-//PrintTriangle();
-Console.ReadLine();
-Magic();
+
+
+Console.Write("Введите количество строк треугольника Паскаля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ReleaseTask(n);
